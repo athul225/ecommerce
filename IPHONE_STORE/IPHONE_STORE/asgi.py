f2e76977c1d5ec980956de2d2b7a84594dd4dcdb
athul225 <athulviju599@gmail.com>
2025-03-12 13:41:39 +0530

    newdocument

---
+++ IPHONE_STORE/IPHONE_STORE/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for IPHONE_STORE project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IPHONE_STORE.settings')
+
+application = get_asgi_application()
